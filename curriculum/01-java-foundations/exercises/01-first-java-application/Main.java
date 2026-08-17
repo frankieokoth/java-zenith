77a3ca2e603f6e13b5b815ee2e505f41a4a25782
Frankie Okoth <114023703+frankieokoth@users.noreply.github.com>
2026-08-17 11:38:04 +0300

    feat: implement different comment styles i.e. multiline and single-line

--- curriculum/01-java-foundations/exercises/01-first-java-application/Main.java
+++ curriculum/01-java-foundations/exercises/01-first-java-application/Main.java
@@ -1,6 +1,7 @@
-/*This is a simple Java program*/
+/*This is a simple Java program.
+Call this program Main.java*/
 public class Main {
-    /*This program begins with a call to main() method*/
+//This program begins with a call to main() method
     public static void main(String[] args) {
         System.out.println("Java Zenith has begun!");
     }
