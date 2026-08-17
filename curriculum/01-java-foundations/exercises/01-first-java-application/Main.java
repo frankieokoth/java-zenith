00aae3e2111e62298dc8ab91a4693f2a681486dc
Frankie Okoth <114023703+frankieokoth@users.noreply.github.com>
2026-08-17 11:10:46 +0300

    add comments to code block

--- curriculum/01-java-foundations/exercises/01-first-java-application/Main.java
+++ curriculum/01-java-foundations/exercises/01-first-java-application/Main.java
@@ -1,4 +1,6 @@
+/*This is a simple Java program*/
 public class Main {
+    /*This program begins with a call to main() method*/
     public static void main(String[] args) {
         System.out.println("Java Zenith has begun!");
     }
