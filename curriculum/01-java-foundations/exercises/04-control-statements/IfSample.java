43d4c63ed746c5839dbdf4915c4fc62584a3bdf7
Frankie Okoth <114023703+frankieokoth@users.noreply.github.com>
2026-08-18 10:58:37 +0300

    feat(if): add first sample of the if statement

---
+++ curriculum/01-java-foundations/exercises/04-control-statements/IfSample.java
@@ -0,0 +1,27 @@
+/*Demonstrate the if.
+Call the file "IfSample.java"
+*/
+class IfSample {
+    public static void main(String [] args) {
+    int x, y;
+
+    x = 10;
+    y = 20;
+
+    if (x < y)
+    System.out.println ("x is less than y");
+
+    x = x * 2;
+    if (x == y)
+    System.out.println ("x now equal to y");
+
+    x = x * 2;
+    if (x > y)
+    System.out.println ("x now greater than y");
+
+    // this won't build anything
+    if (x == y)
+    System.out.println ("you won't see this"); 
+    
+    }
+}
