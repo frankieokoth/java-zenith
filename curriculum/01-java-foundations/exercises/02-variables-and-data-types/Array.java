69c43ddc156832f525bb4ea398bf350058197c79
Frankie Okoth <114023703+frankieokoth@users.noreply.github.com>
2026-09-04 11:47:56 +0300

    feat(arr): demonstrate a one-dimensional array

---
+++ curriculum/01-java-foundations/exercises/02-variables-and-data-types/Array.java
@@ -0,0 +1,22 @@
+// Demonstrate a one-dimensional array
+class Array {
+    public static void main(String[] args) {
+        int [] month_days;
+        month_days = new int [12];
+        month_days [0] = 31;
+        month_days [1] = 31;
+        month_days [2] = 31;
+        month_days [3] = 31;
+        month_days [4] = 31;
+        month_days [5] = 31;
+        month_days [6] = 31;
+        month_days [7] = 31;
+        month_days [8] = 31;
+        month_days [9] = 31;
+        month_days [10] = 31;
+        month_days [11] = 31;
+        
+        System.out.println("April has " + month_days[3] + " days.");;
+    }
+}
+
